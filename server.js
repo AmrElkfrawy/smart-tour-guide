9afa915c3f65fa47322216fd3edc741f870710de
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2024-02-24 23:03:35 +0200

    feat: Establish connection to MongoDB database

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+const mongoose = require('mongoose');
+
 // loading environment variables
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
 const app = require('./app');
 
+const dbConnection = process.env.DB_CONNECTION_STRING;
+mongoose.connect(dbConnection).then(() => {
+    console.log('DB connection successful');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Starting server on port ' + port);
+    console.log('Starting server on port ' + port);
 });
