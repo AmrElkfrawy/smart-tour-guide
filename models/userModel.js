086f9e5ebc52a0e417280a4a5461bc740da04836
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2024-02-25 21:46:51 +0200

    feat: Add user model

---
+++ models/userModel.js
@@ -0,0 +1,58 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name!'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    photo: {
+        type: String,
+        default: 'default.jpg',
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8,
+        select: false,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not the same!',
+        },
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false,
+    },
+    interests: {
+        type: [String],
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
