e7a506abb7da3a4eec4aba172deefed9d2aa3539
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-02-25 22:57:03 +0200

    Added landmark and review models

---
+++ models/reviewModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to the user who left the review
+    required: true,
+  },
+  landmark: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Landmark', // Reference to the landmark being reviewed
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: [1, 'Rating must be at least 1'],
+    max: [5, 'Rating must be at most 5'],
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
