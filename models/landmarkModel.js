6330d8f24655dc17897c0da146caf748a5ac7b93
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-02-25 23:21:24 +0200

    modified category

--- models/landmarkModel.js
+++ models/landmarkModel.js
@@ -2,57 +2,63 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const landmarkSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A landmark must have a name'],
-    unique: true,
-    minLength: [
-      5,
-      'A landmark name must have more than or equal to 5 characters',
-    ],
-    maxLength: [
-      60,
-      'A landmark name must have less than or equal to 60 characters',
-    ],
-    validate: [validator.isAlpha, 'landmark name must only contain characters'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A landmark must have an image cover'],
-  },
-  category: {
-    type: String,
-    enum: ['historical', 'religious', 'natural', 'other'],
-    required: [true, 'A landmark must have a category'],
-  },
-  location: {
-    type: String,
-  },
-
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 0,
-    min: [1, 'Rating must be at least 1'],
-    max: [5, 'Rating must be at most 5'],
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  reviews: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Review', // Reference to reviews associated with the landmark
+    name: {
+        type: String,
+        required: [true, 'A landmark must have a name'],
+        unique: true,
+        minLength: [
+            5,
+            'A landmark name must have more than or equal to 5 characters',
+        ],
+        maxLength: [
+            60,
+            'A landmark name must have less than or equal to 60 characters',
+        ],
     },
-  ],
+    description: {
+        type: String,
+        trim: true,
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A landmark must have an image cover'],
+    },
+    category: {
+        type: String,
+        enum: [
+            'Archaeological Areas',
+            'Temples',
+            'Mosques',
+            'Churches',
+            'Museums',
+            'Tourist Attractions',
+        ],
+        required: [true, 'A landmark must have a category'],
+    },
+    location: {
+        type: String,
+    },
+
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 0,
+        min: [1, 'Rating must be at least 1'],
+        max: [5, 'Rating must be at most 5'],
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0,
+    },
+    reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Review', // Reference to reviews associated with the landmark
+        },
+    ],
 });
 
 const Landmark = mongoose.model('Landmark', landmarkSchema);
