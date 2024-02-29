24d4aab98e3caf63f9542ae488723d59ba5dece8
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-02-29 23:18:39 +0200

    Implemented category endpoints

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -18,3 +18,44 @@ exports.getAllCategories = catchAsync(async (req, res, next) => {
         },
     });
 });
+
+exports.createCategory = catchAsync(async (req, res, next) => {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json({
+        status: 'success',
+        data: {
+            category: newCategory,
+        },
+    });
+});
+
+exports.updateCategory = catchAsync(async (req, res, next) => {
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+
+    if (!category) {
+        return next(new AppError('No category found with this ID', 404));
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            category,
+        },
+    });
+});
+
+exports.deleteCategory = catchAsync(async (req, res, next) => {
+    const category = await Category.findByIdAndDelete(req.params.id);
+
+    if (!category) {
+        return next(new AppError('No category found with this ID', 404));
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    });
+});
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,6 +3,14 @@ const categoryController = require('./../controllers/categoryController');
 
 const router = express.Router();
 
-router.route('/').get(categoryController.getAllCategories);
+router
+    .route('/')
+    .get(categoryController.getAllCategories)
+    .post(categoryController.createCategory);
+
+router
+    .route('/:id')
+    .patch(categoryController.updateCategory)
+    .delete(categoryController.deleteCategory);
 
 module.exports = router;
