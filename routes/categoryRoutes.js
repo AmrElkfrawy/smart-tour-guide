a0ae02dadb837e69a59d16227320e0408c78df83
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-03-01 00:09:42 +0200

    Added restriction to category endpoints

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const categoryController = require('./../controllers/categoryController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router
     .route('/')
     .get(categoryController.getAllCategories)
-    .post(categoryController.createCategory);
+    .post(
+        authController.protect,
+        authController.restrictTo('admin'),
+        categoryController.createCategory
+    );
 
 router
     .route('/:id')
-    .patch(categoryController.updateCategory)
-    .delete(categoryController.deleteCategory);
+    .patch(
+        authController.protect,
+        authController.restrictTo('admin'),
+        categoryController.updateCategory
+    )
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin'),
+        categoryController.deleteCategory
+    );
 
 module.exports = router;
