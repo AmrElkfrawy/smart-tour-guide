cb80e5ee95f1da327ee92bfdb24b70f2dadee83e
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2024-03-07 17:58:47 +0200

    feat: Add error handling for uncaught exceptions and unhandled rejections

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+    console.log('Uncaught exception, Application is terminating.');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 // loading environment variables
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
@@ -7,11 +13,24 @@ dotenv.config({ path: './.env' });
 const app = require('./app');
 
 const dbConnection = process.env.DB_CONNECTION_STRING;
-mongoose.connect(dbConnection).then(() => {
-    console.log('DB connection successful');
-});
+mongoose
+    .connect(dbConnection)
+    .then(() => {
+        console.log('DB connection successful');
+    })
+    .catch((err) => {
+        console.log(`Error connecting to the database ${err}`);
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Starting server on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log('Unhandled rejection, Application is terminating.');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
