ec6fec85806e75dc6a39d4eccc3af534027dfa64
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2024-03-11 11:35:19 +0200

    refactor: update get enpoint in landmark

--- routes/landmarkRoutes.js
+++ routes/landmarkRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 
 const reviewRouter = require('./reviewRoutes');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router.route('/').get(landmarkController.getAllLandmarks).post(
     // authController.protect,
