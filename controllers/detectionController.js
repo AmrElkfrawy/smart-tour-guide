840ab249cd72267a01f36a3b2bc4108c71a1b07e
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2024-03-21 22:18:59 +0200

    refactor: update detection endpoint and itegrate more models

--- controllers/detectionController.js
+++ controllers/detectionController.js
@@ -17,11 +17,20 @@ const upload = multer({ storage: fileStorage, fileFilter: fileFilter });
 exports.uploadLandmarkPhoto = upload.single('photo');
 
 exports.detect = catchAsync(async (req, res, next) => {
+    console.log('here', req.file);
     let apiUrl;
-    if (req.body.location === 'Alexandria') {
+    console.log('as', req.file.buffer);
+    if (req.body.location.toLowerCase() === 'alexandria') {
         apiUrl =
             'https://api-inference.huggingface.co/models/yotasr/Smart_Tour_Alex_v0.1';
+    } else if (req.body.location.toLowerCase() === 'luxor') {
+        apiUrl =
+            'https://api-inference.huggingface.co/models/yotasr/Smart_Tour_Luxor_v1.0';
+    } else if (req.body.location.toLowerCase() === 'giza') {
+        apiUrl =
+            'https://api-inference.huggingface.co/models/yotasr/Smart_Tour_GizaVersion1.01';
     }
+
     const response = await fetch(apiUrl, {
         headers: {
             Authorization: process.env.MODEL_AUTH_TOKEN,
@@ -29,8 +38,10 @@ exports.detect = catchAsync(async (req, res, next) => {
         method: 'POST',
         body: req.file.buffer,
     });
-    const result = await response.json();
-
+    let result = await response.json();
+    if (req.user.role === 'user') {
+        result = result[0];
+    }
     return res.status(200).json({
         status: 'success',
         data: {
