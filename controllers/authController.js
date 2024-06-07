9271d915624861c653abb4d966b3b2704b614c96
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2024-06-07 16:22:13 +0300

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -182,7 +182,6 @@ exports.login = catchAsync(async (req, res, next) => {
         return next(new AppError('Please provide email and password!', 400));
     }
     const user = await User.findOne({ email }).select('+password');
-
     if (!user || !(await user.correctPassword(password, user.password))) {
         return next(new AppError('Incorrect email or password', 401));
     }
--- models/userModel.js
+++ models/userModel.js
@@ -73,7 +73,7 @@ const userSchema = new mongoose.Schema(
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
 
-    // this.password = await bcrypt.hash(this.password, 12);
+    this.password = await bcrypt.hash(this.password, 12);
     // Delete passwordConfirm field
     this.passwordConfirm = undefined;
     next();
