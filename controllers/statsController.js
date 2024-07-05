87935e353b5e7e3b6113917d095edb1cb552e611
fairouzmagdy <fairouzmagdy219@gmail.com>
2024-07-06 02:43:53 +0300

    Added statistics for admin dashboard

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -5,6 +5,7 @@ const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const CustomizedTour = require('../models/customizedTourModel');
 
 exports.getStats = catchAsync(async (req, res, next) => {
     const users = await User.countDocuments({ role: 'user' });
@@ -13,6 +14,158 @@ exports.getStats = catchAsync(async (req, res, next) => {
     const landmarks = await Landmark.countDocuments();
     const bookings = await Booking.countDocuments();
     const tours = await Tour.countDocuments();
+
+    // Aggregations
+    const monthlySignUps = await User.aggregate([
+        {
+            $match: {
+                createdAt: {
+                    $gte: new Date(
+                        new Date().setFullYear(new Date().getFullYear() - 1)
+                    ),
+                },
+            },
+        },
+        {
+            $group: {
+                _id: {
+                    year: { $year: '$createdAt' },
+                    month: { $month: '$createdAt' },
+                },
+                numUsers: { $sum: 1 },
+            },
+        },
+        {
+            $sort: { _id: 1 },
+        },
+    ]);
+
+    const monthlyRevenue = await Booking.aggregate([
+        {
+            $group: {
+                _id: {
+                    $month: '$createdAt',
+                },
+                totalRevenue: { $sum: '$totalPrice' },
+            },
+        },
+        {
+            $project: {
+                _id: 0, // Exclude the default _id field
+                month: '$_id', // Rename _id to month
+                totalRevenue: 1, // Include totalRevenue field
+            },
+        },
+        {
+            $sort: { _id: 1 },
+        },
+    ]);
+
+    const revenueStats = await Booking.aggregate([
+        {
+            $group: {
+                _id: null,
+                totalRevenue: { $sum: '$totalPrice' },
+                averageRevenue: { $avg: '$totalPrice' },
+            },
+        },
+    ]);
+
+    const popularLandmarks = await CustomizedTour.aggregate([
+        {
+            $unwind: '$landmarks',
+        },
+        {
+            $group: {
+                _id: '$landmarks',
+                numBookings: { $sum: 1 },
+            },
+        },
+        {
+            $sort: { numBookings: -1 },
+        },
+        {
+            $lookup: {
+                from: 'landmarks',
+                localField: '_id',
+                foreignField: '_id',
+                as: 'landmark',
+            },
+        },
+        {
+            $unwind: '$landmark',
+        },
+        {
+            $project: {
+                name: '$landmark.name',
+                numBookings: 1,
+            },
+        },
+        {
+            $limit: 5,
+        },
+    ]);
+
+    const topGuides = await Booking.aggregate([
+        { $unwind: '$tours' },
+        {
+            $group: {
+                _id: '$tours.guide',
+                bookingCount: { $sum: 1 },
+            },
+        },
+        {
+            $lookup: {
+                from: 'users',
+                localField: '_id',
+                foreignField: '_id',
+                as: 'guide',
+            },
+        },
+        { $unwind: '$guide' },
+        {
+            $project: {
+                _id: 0,
+                name: '$guide.name',
+                rating: '$guide.rating',
+                ratingsQuantity: '$guide.ratingsQuantity',
+                bookingCount: '$bookingCount',
+            },
+        },
+        { $sort: { bookingCount: -1 } },
+        { $limit: 5 },
+    ]);
+
+    const topBookedTours = await Booking.aggregate([
+        { $unwind: '$tours' },
+        {
+            $group: {
+                _id: '$tours.tour',
+                bookingCount: { $sum: 1 },
+            },
+        },
+        {
+            $lookup: {
+                from: 'tours',
+                localField: '_id',
+                foreignField: '_id',
+                as: 'tour',
+            },
+        },
+        { $unwind: '$tour' },
+        {
+            $project: {
+                _id: 0,
+                name: '$tour.name',
+                rating: '$tour.rating',
+                ratingsQuantity: '$tour.ratingsQuantity',
+                bookingCount: '$bookingCount',
+            },
+        },
+        { $sort: { bookingCount: -1 } },
+        { $limit: 5 },
+    ]);
+
     return res.status(200).json({
         status: 'success',
         docs: {
@@ -22,6 +175,13 @@ exports.getStats = catchAsync(async (req, res, next) => {
             landmarks,
             tours,
             bookings,
+            monthlySignUps,
+            popularLandmarks,
+            topBookedTours,
+            topGuides,
+            monthlyRevenue,
+            totalRevenue: revenueStats[0]?.totalRevenue || 0,
+            averageRevenue: revenueStats[0]?.averageRevenue || 0,
         },
     });
 });
